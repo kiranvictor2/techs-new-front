aa2f119820d83d2a77e990bfc456999396e1683d
kiran victor <kiranvictor2@gmail.com>
2025-10-10 10:47:09 +0530

    dhaid

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-// const BASE_URL = "http://15.207.236.246/api/"; // <-- Your backend URL
-const BASE_URL = "http://localhost:8000"; // <-- Your backend URL
+const BASE_URL = "http://15.207.236.246/api/"; // <-- Your backend URL
+// const BASE_URL = "http://localhost:8000"; // <-- Your backend URL
 export default BASE_URL;
