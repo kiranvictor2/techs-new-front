011f46bbb025817177f827d058102c676df7fcc2
kiran victor <kiranvictor2@gmail.com>
2025-10-10 11:31:44 +0530

    djhddddd

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-const BASE_URL = "http://15.207.236.246/api/"; // <-- Your backend URL
+// const BASE_URL = "http://15.207.236.246/api/"; // <-- Your backend URL
 // const BASE_URL = "http://localhost:8000"; // <-- Your backend URL
+const BASE_URL = "https://maxesmanual.com/api/"; // <-- Your backend URL
 export default BASE_URL;
